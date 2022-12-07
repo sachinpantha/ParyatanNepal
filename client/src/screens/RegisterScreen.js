c352345e815c90d0029ce37c83d0022c2bc11c75
Sachiiiiin <sachinpantha69@gmail.com>
2022-12-07 14:11:48 +0545

    cloing

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -109,7 +109,7 @@ const RegisterScreen = () => {
       <Row className="py-3">
         <Col>
           Have an Account?{" "}
-          <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+          <Link style={{ color: "red" }} to={redirect ? `/login?redirect=${redirect}` : "/login"}>
             Login
           </Link>
         </Col>
